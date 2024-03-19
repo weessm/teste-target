f63b1b01d5fd71fc3cb440a6e7917cb1929c2c14
Weslei Miranda <wsmiranda11@gmail.com>
2024-03-19 09:24:52 -0300

    feat: questao 1

---
+++ 1_teste.js
@@ -0,0 +1,14 @@
+let INDICE = 13;
+let SOMA = 0;
+let K = 0;
+
+while (K < INDICE) {
+  K = K + 1;
+  console.log("valor K: " + K);
+  SOMA = SOMA + K;
+  console.log("valor soma: " + SOMA);
+}
+
+console.log(SOMA);
+
+//O valor da variável SOMA será 91
